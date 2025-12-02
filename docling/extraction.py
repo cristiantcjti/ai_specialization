99288e715ec238a78fd02f92e43833c3108ebb19
cristian <cristiantcjti@gmail.com>
2025-12-02 20:47:36 -0300

    feat[AI-004]

---
+++ docling/extraction.py
@@ -0,0 +1,12 @@
+from docling.document_converter import DocumentConverter
+
+converter = DocumentConverter()
+
+# result = converter.convert("./2408.09869v5.pdf")
+result = converter.convert("https://arxiv.org/pdf/2408.09869")
+
+document = result.document
+
+markdown_output = document.export_to_markdown()
+
+print(markdown_output)
